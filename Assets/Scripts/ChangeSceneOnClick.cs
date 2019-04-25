b864c8893f6caf1c3bd380bcb950fd1d0ab3aff6
Wouter Huygen <wouter.huygen@hotmail.be>
2019-04-25 17:24:49 +0200

    Add basic UI

---
+++ Assets/Scripts/ChangeSceneOnClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeSceneOnClick : MonoBehaviour
+{
+    public Button buttonToClick;
+    public string sceneToLoad;
+
+    void Start()
+    {
+        //Calls the TaskOnClick method when you click the Button
+        buttonToClick.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+    }
+}
